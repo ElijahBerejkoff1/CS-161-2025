e11a4b46b93cc03f6ce28ff34769254aa78165a3
ElijahBerejkoff1 <eberejkoff@cocc.edu>
2025-01-09 20:03:32 -0800

    HW #1
    Homework using f strings. 1/9/25

---
+++ BerejkoffElijahWeek1AssingmentCIS161.py
@@ -0,0 +1,23 @@
+
+print ("Hello World!")
+#Define Var for my pet and his name
+pet = "Dog"
+petName = "Thor"
+print (f"This is a f string about my {pet} named {petName}") #using lines 2 and 3 to say what my pet is and what is name is in one sentance
+print(f"My pet and petname is {pet + petName}... kinda silly") #having fun with it!
+print() #skipping a line for orginization
+myname = "Elijah"
+myage = 18
+mysavings = 29179465 #Yup im that rich 100%
+print (f"Hello {myname}! Right now you are {myage}. But in 42 years you will be {myage + 42}.") #Inputting my name, and age +42
+print(f"if you save {mysavings} every year in 3 years you will be worth {mysavings * 3} dollars!") #Seeing how rich I totally am!
+#defining how many seconds/min/hours/days there are in a month
+seconds = 60
+minute = 60
+hour = 24
+day = 31
+print(f"The amount of seconds in a month is {seconds * minute * hour * day}.") #multiplying them to find the amount of seconds in a month
+print()
+eggs = 85 #defining how many eggs we have
+print(f"With {eggs} you can make {eggs // 12} dozen eggs with {eggs % 12} left over.") #how many dozen eggs can you make with remainder.
+#Tried figuring out the libraries thing but got stuck at the adding/subtracting variables (that arnt a specific number) part :(
